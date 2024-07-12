42704b2e059e1e3a1d17f5aad28ef2f63e8567a8
NSandEP <starart2004@mail.ru>
2024-07-12 15:56:28 +0300

    Add files via upload

--- Task 4.py
+++ Task 4.py
@@ -1,3 +1,8 @@
 # 4th program
-print(13.42, 42.13)
-print(int(13.42) == 42.13 * 100 - 42 * 100 or int(42.13) == 13.42 * 100 - 13 * 100 or 13.42 == 42.13)
+a, b = 13.42, 42.13
+print(a, b)
+aint, bint = int(a), int(b)
+print(aint, bint)
+afl, bfl = int((a - aint) * 100), int((b - bint) * 100)
+print(afl, bfl)
+print(aint == bfl or bint == afl)
